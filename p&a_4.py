ba6d226db7dc08ab685a56eaf2a689d5a6510121
Sesha Sai Sreevani Kappagantula <55071795+sree14-shy@users.noreply.github.com>
2019-10-05 01:21:36 -0400

    Add files via upload

---
+++ p&a_4.py
@@ -0,0 +1,117 @@
+# Perceptron and Adaline model implementation for separable classes with 4 features considered
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+class Perceptron(object):
+   def __init__(self, rate = 0.01, niter = 10):
+      self.rate = rate
+      self.niter = niter
+
+   def fit(self, X, y):
+      """Fit training data
+      X : Training vectors, X.shape : [#samples, #features]
+      y : Target values, y.shape : [#samples]
+      """
+
+      # weights
+      self.weight = np.zeros(1 + X.shape[1])
+
+      # Number of misclassifications
+      self.errors = []  # Number of misclassifications
+
+      for i in range(self.niter):
+         err = 0
+         for xi, target in zip(X, y):
+            delta_w = self.rate * (target - self.predict(xi))
+            self.weight[1:] += delta_w * xi
+            self.weight[0] += delta_w
+            err += int(delta_w != 0.0)
+         self.errors.append(err)
+      return self
+
+   def net_input(self, X):
+      """Calculate net input"""
+      return np.dot(X, self.weight[1:]) + self.weight[0]
+
+   def predict(self, X):
+      """Return class label after unit step"""
+      return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+class AdalineGD(object):
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    def activation(self, X):
+        return self.net_input(X)
+    def predict(self, X):
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+  
+#initialize dataset,X and y values
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0,1,2,3]].values
+
+
+#epochs vs misclassifications graph
+print('error vs epochs graph for perceptron')
+pn = Perceptron()
+pn.fit(X, y)
+plt.plot(range(1, len(pn.errors) + 1), pn.errors, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+plt.show()
+
+
+#metrics calculation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+ppn = Perceptron()
+ppn.fit(X_train, y_train)
+y_pred = ppn.predict(X_test)
+
+#printing the results 
+print('Metrics for perceptron classifier\n\nMisclassified samples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+print('Confusion matrix: \n%s' % confusion_matrix(y_test, y_pred))
+
+#metrics calculation
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', 0, 1)
+X = df.iloc[0:100, [0,1,2,3]].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+from mlxtend.classifier import Adaline
+ppn = Adaline(epochs=20,eta=0.1, random_seed=1)
+
+ppn.fit(X_train, y_train)
+y_pred = ppn.predict(X_test)
+
+#printing the results 
+print('\nMetrics for Adaline classifier\n\nMisclassified samples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+print('Confusion matrix: \n%s' % confusion_matrix(y_test, y_pred))
